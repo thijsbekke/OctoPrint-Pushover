3659fb029800da3411b1a71e09acf0d011320a49
thijsbekke <thijsbekke@gmail.com>
2016-12-08 23:33:44 +0100

    #6 added the option to test the notification, had too rewrite the error handling for this.

--- octoprint_pushover/__init__.py
+++ octoprint_pushover/__init__.py
@@ -15,34 +15,60 @@ __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agp
 __copyright__ = "Released under terms of the AGPLv3 License"
 __plugin_name__ = "Pushover"
 
+import flask
+import octoprint.plugin
 
 class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 					 octoprint.plugin.SettingsPlugin,
 					 octoprint.plugin.StartupPlugin,
-					 octoprint.plugin.TemplatePlugin):
+					 octoprint.plugin.SimpleApiPlugin,
+					 octoprint.plugin.TemplatePlugin,
+					 octoprint.plugin.AssetPlugin):
 	user_key = ""
 	api_url = "api.pushover.net:443"
 
+	def get_assets(self):
+		return {
+			"js": ["js/pushover.js"]
+		}
+
+	def get_api_commands(self):
+		return dict(
+			test=["api_key", "user_key"]
+		)
+
+	def on_api_command(self, command, data):
+		if command == "test":
+			payload = {}
+			payload["message"] = "pewpewpew!! OctoPrint works."
+			payload["token"] = data["api_key"]
+			payload["user"] = data["user_key"]
+
+			if "sound" in data:
+				payload["sound"] = data["sound"]
+			try:
+				self.validate_pushover(data["user_key"])
+				self.event_message(payload)
+				return flask.jsonify(dict(success=True))
+			except Exception as e:
+				return flask.jsonify(dict(success=False, msg=str(e.message)))
+		return flask.make_response("Unknown command", 400)
+
+
 	def validate_pushover(self, user_key):
 		if not user_key:
-			self._logger.exception("No user key provided")
-			return False
+			raise ValueError("No user key provided")
 
 		self.user_key = user_key
 		try:
 			HTTPResponse = self.post("users/validate.json", self.create_payload({}))
 
-			if not HTTPResponse:
-				self._logger.exception("HTTPResponse is false")
-
 			status = HTTPResponse.getheader('status')
 			if status is not None and status.startswith('40'):
-				self._logger.exception("Error while instantiating Pushover, header %s" % HTTPResponse.getheader("status"))
-				return False
+				raise ValueError("Error while instantiating Pushover, header %s" % HTTPResponse.getheader("status"))
 			elif status is not None and not status.startswith('200'):
-				self._logger.exception(
+				raise ValueError(
 					"error while instantiating Pushover, header %s" % HTTPResponse.getheader("status"))
-				return False
 
 			response = json.loads(HTTPResponse.read())
 
@@ -51,7 +77,7 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 				return True
 
 		except Exception, e:
-			self._logger.exception("error while instantiating Pushover: %s" % str(e))
+			raise ValueError("error while instantiating Pushover: %s" % str(e))
 
 		return False
 
@@ -69,11 +95,6 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 	def PrintFailed(self, payload):
 		file = os.path.basename(payload["file"])
 		return self._settings.get(["events", "PrintFailed", "message"]).format(**locals())
-
-	def Error(self, payload):
-		error = payload["error"]
-		return self._settings.get(["events", "Error", "message"]).format(**locals())
-
 
 	def on_event(self, event, payload):
 		# Does the event exists in the settings ?
@@ -113,24 +134,32 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 			import socket
 			payload["url"] = "http://%s" % socket.getfqdn()
 
-		# If no sound parameter is specified, the user"s default tone will play.
-		# If the user has not chosen a custom sound, the standard Pushover sound will play.
-		sound = self._settings.get(["sound"])
-		if sound:
-			payload["sound"] = sound
-
-		self.post("messages.json", self.create_payload(payload))
+		if "sound" not in payload:
+			# If no sound parameter is specified, the user"s default tone will play.
+			# If the user has not chosen a custom sound, the standard Pushover sound will play.
+			sound = self._settings.get(["sound"])
+			if sound:
+				payload["sound"] = sound
+		try:
+			self.post("messages.json", self.create_payload(payload))
+		except Exception, e:
+			self._logger.exception(str(e))
 
 	def on_after_startup(self):
-		self.validate_pushover(self._settings.get(["user_key"]))
+		try:
+			self.validate_pushover(self._settings.get(["user_key"]))
+		except Exception, e:
+			self._logger.exception(str(e))
 
 	def on_settings_save(self, data):
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-
-		import threading
-		thread = threading.Thread(target=self.validate_pushover, args=(self._settings.get(["user_key"]),))
-		thread.daemon = True
-		thread.start()
+		try:
+			import threading
+			thread = threading.Thread(target=self.validate_pushover, args=(self._settings.get(["user_key"]),))
+			thread.daemon = True
+			thread.start()
+		except Exception, e:
+			self._logger.exception(str(e))
 
 	def on_settings_load(self):
 		data = octoprint.plugin.SettingsPlugin.on_settings_load(self)
@@ -148,10 +177,12 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 		return dict(admin=[["token"], ["user_key"]])
 
 	def create_payload(self, create_payload):
-		x = {
-			"token": self._settings.get(["token"]),
-			"user": self.user_key
-		}
+		x = {}
+		if "token" not in create_payload:
+			x["token"] = self._settings.get(["token"])
+
+		if "user" not in create_payload:
+			x["user"] = self.user_key
 
 		new_payload = x.copy()
 		new_payload.update(create_payload)
@@ -164,8 +195,7 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 			conn.request("POST", "/1/" + uri, payload, {"Content-type": "application/x-www-form-urlencoded"})
 			return conn.getresponse()
 		except Exception, e:
-			self._logger.exception("error while instantiating Pushover: %s" % str(e))
-			return False
+			raise ValueError("error while instantiating Pushover: %s" % str(e))
 
 	def get(self, uri):
 		try:
@@ -175,8 +205,7 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 			return conn.getresponse()
 
 		except Exception, e:
-			self._logger.exception("error while getting data from Pushover: %s" % str(e))
-			return False
+			raise ValueError("error while getting data from Pushover: %s" % str(e))
 
 	def get_settings_defaults(self):
 		return dict(
@@ -205,19 +234,19 @@ class PushoverPlugin(octoprint.plugin.EventHandlerPlugin,
 		)
 
 	def get_sounds(self):
-		# Make a call too the sounds API
-		HTTPResponse = self.get("sounds.json")
-
-		if not HTTPResponse:
+		try:
+			# Make a call too the sounds API
+			HTTPResponse = self.get("sounds.json")
+		except Exception, e:
+			self._logger.exception(str(e))
 			return
 
 		return json.loads(HTTPResponse.read())["sounds"]
 
 	def get_template_configs(self):
 		return [
-			dict(type="settings", name="Pushover", custom_bindings=False)
+			dict(type="settings", name="Pushover", custom_bindings=True)
 		]
-
 
 	def get_update_information(self):
 		return dict(
---
+++ octoprint_pushover/static/js/pushover.js
@@ -0,0 +1,57 @@
+$(function() {
+    function PushoverViewModel(parameters) {
+        var self = this;
+
+        self.settingsViewModel = parameters[1];
+
+        self.testActive = ko.observable(false);
+        self.testResult = ko.observable(false);
+        self.testSuccessful = ko.observable(false);
+        self.testMessage = ko.observable();
+
+        self.testNotification  = function() {
+            self.testActive(true);
+            self.testResult(false);
+            self.testSuccessful(false);
+            self.testMessage("");
+
+            var apikey = $('#apikey').val();
+            var userkey = $('#userkey').val();
+            var sound = $('#pushover-sound option:selected').text();
+
+            var payload = {
+                command: "test",
+                api_key: apikey,
+                user_key: userkey,
+                sound: sound,
+            };
+
+            $.ajax({
+                url: API_BASEURL + "plugin/pushover",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify(payload),
+                contentType: "application/json; charset=UTF-8",
+                success: function(response) {
+                    self.testResult(true);
+                    self.testSuccessful(response.success);
+                    if (!response.success && response.hasOwnProperty("msg")) {
+                        self.testMessage(response.msg);
+                    } else {
+                        self.testMessage(undefined);
+                    }
+                },
+                complete: function() {
+                    self.testActive(false);
+                }
+            });
+        };
+
+        self.onBeforeBinding = function() {
+            self.settings = self.settingsViewModel.settings;
+        };
+    }
+
+    // view model class, parameters for constructor, container to bind to
+    ADDITIONAL_VIEWMODELS.push([PushoverViewModel, ["loginStateViewModel", "settingsViewModel"], document.getElementById("settings_plugin_pushover")]);
+});
